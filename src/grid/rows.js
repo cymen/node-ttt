398f2f90aa0916c3b973861b8f549b6cea8bb25b
Cymen Vig <cymenvig@gmail.com>
2014-01-22 00:09:02 -0600

    Finish bringing over row methods

--- src/grid/rows.js
+++ src/grid/rows.js
@@ -1,5 +1,23 @@
 'use strict';
 
+module.exports.diagonal = function(cells) {
+    var left = [],
+        right = [],
+        size = cells.length,
+        length = Math.sqrt(size);
+
+    if (length % 2 === 0) {
+        return new Error('Cannot determine diagonal rows on a grid with even length sides');
+    }
+
+    for (var i = 1; i <= size; i += length + 1) {
+        left.push(cells[i - 1]);
+        right.push(cells[i - 1 + length]);
+    }
+
+    return [left, right];
+};
+
 module.exports.horizontal = function(cells) {
     var rows = [],
         size = cells.length,
@@ -7,6 +25,22 @@ module.exports.horizontal = function(cells) {
 
     for (var i = 0; i < size; i += length) {
         rows.push(cells.slice(i, length));
+    }
+
+    return rows;
+};
+
+module.exports.vertical = function(cells) {
+    var rows = [],
+        size = cells.length,
+        length = Math.sqrt(size);
+
+    for (var x = 0; x < length; x++) {
+        var row = [];
+        for (var y = x; y < size; y += length) {
+            row.push(cells[y]);
+        }
+        rows.push(row);
     }
 
     return rows;
--- src/grid/spec/rows_spec.js
+++ src/grid/spec/rows_spec.js
@@ -1,14 +1,41 @@
 'use strict';
 var rows = require('../rows');
 
-describe('grid rows', function() {
-    describe('horizontal', function() {
-        it('returns rows assuming the array of values is a representation of a grid', function() {
-            var values = [
-                'a', 'b',
-                'c', 'd'
-            ];
-            expect(rows.horizontal(values)).toContain(['a', 'b'], ['c', 'd']);
-        });
+describe('grid rows (assumes provided array represents a square grid)', function() {
+    it('returns diagonal rows', function() {
+        var values = [
+            'a', 'b', 'c',
+            'd', 'e', 'f',
+            'g', 'h', 'i'
+        ];
+
+        expect(rows.diagonal(values)).toContain(['a', 'e', 'i'], ['c', 'e', 'g']);
+    });
+
+    it('returns an Error if attempting to call diagonal rows on a grid with even length size', function() {
+        var values = [
+            'a', 'b',
+            'c', 'd'
+        ];
+
+        expect(rows.diagonal(values)).toMatch(new Error('Cannot determine diagonal rows on a grid with even length sides'));
+    });
+
+    it('returns horizontal rows', function() {
+        var values = [
+            'a', 'b',
+            'c', 'd'
+        ];
+
+        expect(rows.horizontal(values)).toContain(['a', 'b'], ['c', 'd']);
+    });
+
+    it('returns vertical rows', function() {
+        var values = [
+            'a', 'b',
+            'c', 'd'
+        ];
+
+        expect(rows.vertical(values)).toContain(['a', 'c'], ['b', 'd']);
     });
 });
