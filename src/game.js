12a5b18860d85f3b759d01eeba36cbd0556e8283
Cymen Vig <cymenvig@gmail.com>
2014-01-24 01:07:53 -0600

    Add game (WIP)

--- src/game.js
+++ src/game.js
@@ -1,7 +1,37 @@
 'use strict';
 
-var Board = require('./board/board'),
-    boardPrint = require('./board/print');
+var Q = require('q'),
+    scorer = require('./scorer'),
+    print = require('./board/print');
 
-var board = new Board();
-boardPrint.rows(board.horizontal_rows());
+module.exports = {
+    play: function(board, player_x, player_o) {
+        var self = this;
+        return Q.promise(function(resolve) {
+            self
+                .get_play(board, player_x, player_o)
+                .then(function(choice) {
+                    board.set(choice, scorer.turn(board));
+                    if (scorer.is_over(board)) {
+                        resolve();
+                    } else {
+                        resolve(self.play(board, player_x, player_o));
+                    }
+                });
+        });
+    },
+
+    get_play: function(board, player_x, player_o) {
+        var current_player;
+
+        print.rows(board.horizontal_rows()); // TODO this doesn't belong in get_play
+
+        if (scorer.turn(board) === 'x') {
+            current_player = player_x;
+        } else {
+            current_player = player_o;
+        }
+
+        return current_player.play(board.empty_cells());
+    }
+};
--- src/spec/game_spec.js
+++ src/spec/game_spec.js
@@ -1,1 +1,58 @@
+'use strict';
+var Q = require('q'),
+    game = require('../game'),
+    Board = require('../board/board'),
+    next_open_player = require('../player/next_open'),
+    scorer = require('../scorer');
 
+describe('game', function() {
+    it('plays a game with two players', function(done) {
+        var player_x = next_open_player,
+            player_o = next_open_player,
+            board = new Board();
+
+        var promise = game.play(board, player_x, player_o);
+
+        promise.then(
+            function() {
+                expect(scorer.winner(board)).toBe('x');
+                expect(board.horizontal_rows()).toContain(['x', 'o', 'x']);
+                expect(board.horizontal_rows()).toContain(['o', 'x', 'o']);
+                expect(board.horizontal_rows()).toContain(['x', undefined, undefined]);
+                done();
+            },
+            function(error) {
+                expect('reject').toBe('not happening');
+                done();
+            }
+        );
+    });
+
+    it('prompts x for a cell on an empty board', function(done) {
+        var mockPlayer = function() {
+            return {
+                play: jasmine.createSpy().andCallFake(function(choices) {
+                    return Q.resolve(choices[0]);
+                })
+            };
+        };
+        var player_x = mockPlayer(),
+            player_o = mockPlayer(),
+            board = new Board();
+
+        var promise = game.get_play(board, player_x, player_o);
+
+        promise.then(
+            function(choice) {
+                expect(player_x.play).toHaveBeenCalledWith([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+                expect(choice).toBe(1);
+                expect(player_o.play).not.toHaveBeenCalled();
+                done();
+            },
+            function(error) {
+                expect('reject').toBe('not happening');
+                done();
+            }
+        );
+    });
+});
