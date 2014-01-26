90b5ba10785fd63b6f9d951b386635c35a0cdbe6
Cymen Vig <cymenvig@gmail.com>
2014-01-26 01:08:37 -0600

    Add a computer player using negamax

--- src/player/computer.js
+++ src/player/computer.js
@@ -1,8 +1,84 @@
 'use strict';
-var shuffle = require('mess');
-
-var OPTIMAL_CELLS = [1, 3, 5, 7, 9];
+var Q = require('q'),
+    scorer = require('../scorer'),
+    print = require('../board/print'),
+    OPTIMAL_CELLS = [1, 3, 5, 7, 9];
 
 module.exports = {
-    play: function(cells) {}
+    play: function(board) {
+        var moves = this.get_best_moves(board),
+            move = moves[Math.floor(Math.random() * moves.length)];
+        return Q.resolve(move);
+    },
+
+    get_best_moves: function(board) {
+        var self = this,
+            weights = {},
+            myMark = scorer.turn(board),
+            bestResult = -Infinity,
+            result,
+            move;
+
+        if (board.empty_cells().length === 9) {
+            return OPTIMAL_CELLS;
+        }
+
+        board.empty_cells().forEach(function(cell) {
+            board.set(cell, myMark);
+
+            result = -self.negamax(board, self.opponent(myMark), board.CELL_COUNT);
+            if (result > bestResult) {
+                bestResult = result;
+            }
+            if (weights[result]) {
+                weights[result].push(cell);
+            } else {
+                weights[result] = [cell];
+            }
+
+            board.unset(cell);
+        });
+
+        return weights[bestResult];
+    },
+
+    analysis: function(board, player, height) {
+        if (scorer.is_won(board)) {
+            if (scorer.winner(board) === player) {
+                return height;
+            } else {
+                return -height;
+            }
+        }
+
+        return 0;
+    },
+
+    negamax: function(board, player, height) {
+        var self = this,
+            bestWeight = -Infinity,
+            playResult,
+            endingPlayer;
+
+        if (scorer.is_over(board)) {
+            return this.analysis(board, player, height);
+        }
+
+        board.empty_cells().forEach(function(cell) {
+            board.set(cell, player);
+
+            playResult = -self.negamax(board, self.opponent(player), height - 1);
+            bestWeight = Math.max(playResult, bestWeight);
+
+            board.unset(cell);
+        });
+
+        return bestWeight;
+    },
+
+    opponent: function(player) {
+        return (player === 'x') ? 'o' : 'x';
+    },
+
+    type: 'computer'
 };
--- src/player/spec/computer_spec.js
+++ src/player/spec/computer_spec.js
@@ -1,1 +1,106 @@
+'use strict';
+var computer = require('../computer'),
+    Board = require('../../board/board'),
+    ___;
 
+describe('computer', function() {
+    it('has a type', function() {
+        expect(computer.type).toBe('computer');
+    });
+
+    it('can determine the opponent', function() {
+        expect(computer.opponent('x')).toBe('o');
+        expect(computer.opponent('o')).toBe('x');
+    });
+
+    it('returns a promise', function(done) {
+        var board = new Board([
+            'x', 'o', 'x',
+            'o', ___, ___,
+            'x', ___, 'o'
+        ]);
+
+        var promise = computer.play(board);
+
+        promise.then(
+            function(move) {
+                expect(move).toBe(5);
+                done();
+            },
+            function() {
+                expect('promise').toBe('resolved');
+                done();
+            }
+        );
+    });
+
+    describe('best_choices', function() {
+        it('should always choose one of 1, 3, 5, 7, 9 on an empty board', function() {
+            var board = new Board();
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([1, 3, 5, 7, 9]);
+        });
+
+        it('blocks a win by opponent', function() {
+            var board = new Board([
+                ___, 'x', 'x',
+                ___, 'o', 'x',
+                ___, ___, 'o'
+            ]);
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([1]);
+        });
+
+        it('blocks a win by opponent', function() {
+            var board = new Board([
+                'x', ___, ___,
+                'x', 'o', ___,
+                'o', ___, ___
+            ]);
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([3]);
+        });
+
+        it('attempts to setup a fork', function() {
+            var board = new Board([
+                'x', ___, ___,
+                ___, ___, ___,
+                ___, ___, 'o'
+            ]);
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([3, 7]);
+        });
+
+        it('goes for the fork', function() {
+            var board = new Board([
+                'x', ___, ___,
+                'o', ___, ___,
+                'x', ___, 'o'
+            ]);
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([3]);
+        });
+
+        it('picks a win over blocking the opponent', function() {
+            var board = new Board([
+                'x', ___, 'x',
+                ___, ___, ___,
+                'o', ___, 'o'
+            ]);
+
+            var best_choices = computer.get_best_moves(board);
+
+            expect(best_choices).toEqual([2]);
+        });
+    });
+});
