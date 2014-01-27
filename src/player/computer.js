7676ef79bd9286de55125bead64d206df9b865ec
Cymen Vig <cymenvig@gmail.com>
2014-01-27 10:02:22 -0500

    Minor cleanup of computer player

--- src/player/computer.js
+++ src/player/computer.js
@@ -1,15 +1,21 @@
 'use strict';
+
 var Q = require('q'),
     constants = require('./constants'),
     scorer = require('../scorer'),
     OPTIMAL_CELLS = [1, 3, 5, 7, 9];
 
 module.exports = {
-    play: function(board) {
-        var moves = this.get_best_moves(board),
-            random = Math.floor(Math.random() * moves.length);
+    analysis: function(board, player, height) {
+        if (scorer.is_won(board)) {
+            if (scorer.winner(board) === player) {
+                return height;
+            } else {
+                return -height;
+            }
+        }
 
-        return Q.resolve(moves[random]);
+        return 0;
     },
 
     get_best_moves: function(board) {
@@ -40,18 +46,6 @@ module.exports = {
         });
 
         return weights[bestResult];
-    },
-
-    analysis: function(board, player, height) {
-        if (scorer.is_won(board)) {
-            if (scorer.winner(board) === player) {
-                return height;
-            } else {
-                return -height;
-            }
-        }
-
-        return 0;
     },
 
     negamax: function(board, player, height, alpha, beta) {
@@ -87,6 +81,14 @@ module.exports = {
     opponent: function(player) {
         return (player === constants.X) ? constants.O : constants.X;
     },
+
+    play: function(board) {
+        var moves = this.get_best_moves(board),
+            random = Math.floor(Math.random() * moves.length);
+
+        return Q.resolve(moves[random]);
+    },
+
 
     type: 'computer'
 };
